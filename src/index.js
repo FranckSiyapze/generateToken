d14f44d6ed774dc76f9512a15567b6b730193d21
Franck SIYAPZE <sfranckdabryn@gmail.com>
2024-06-08 14:45:16 -0400

    add twillio Whatsapp message api TEST

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ let apiRoutes = require("./api-routes");
 // Use Api routes in the App
 app.use('/api', apiRoutes);
 // Setup server port
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 8000;
 // Send message for default URL
 app.get('/', (req, res) => res.send('Hello World with Express'));
 // Launch app to listen to specified port
